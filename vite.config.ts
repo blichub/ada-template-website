966a532fbb7d3b72f28ed1312a54c84c5ae7fde7
Antonio <antonio.dp.antunes@gmail.com>
2025-12-11 13:27:26 +0100

    undo last commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/ada-template-website/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
