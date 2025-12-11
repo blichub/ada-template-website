ad89bb5ae1736688c5785f7772a3abf1d5662330
Antonio <antonio.dp.antunes@gmail.com>
2025-12-11 13:26:09 +0100

    try

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@
   import path from 'path';
 
   export default defineConfig({
-    base: '/ada-template-website/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
