40fac2b7d355f2eff03cf5fb9bdfb800a4cdfb7b
Nour <nour.benjaafar@epfl.ch>
2025-12-20 14:29:04 +0100

    imports fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 import { Navigation } from "./components/Navigation";
 import { HomePage } from "./components/HomePage";
 import { RQ1Spatial } from "./components/RQ1Spatial";
@@ -7,7 +9,6 @@ import { RQ3CellTypes } from "./components/RQ3CellTypes";
 import { About } from "./components/About";
 import { ScrollToTop } from "./components/ScrollToTop";
 import { DatasetMethods } from "./components/dataset_methods";
-
 export default function App() {
   return (
     <BrowserRouter>
@@ -17,17 +18,17 @@ export default function App() {
         <Navigation />
 
         <Routes>
-          {/* Page d’accueil */}
+          {/* Accueil */}
           <Route path="/" element={<HomePage />} />
 
-          {/* Pages principales */}
+          {/* Pages */}
           <Route path="/dataset-methods" element={<DatasetMethods />} />
           <Route path="/rq1-spatial" element={<RQ1Spatial />} />
           <Route path="/rq2-temporal" element={<RQ2Temporal />} />
           <Route path="/rq3-celltypes" element={<RQ3CellTypes />} />
           <Route path="/about" element={<About />} />
 
-          {/* Sécurité : toute route inconnue → Home */}
+          {/* fallback */}
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </div>
