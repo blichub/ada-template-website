86aa673dd86a91c8bbdf98e619bf0657edd74372
Antonio <antonio.dp.antunes@gmail.com>
2025-12-20 15:59:20 +0100

    trying to change url

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-
+﻿
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "/ada-template-website/",
+  base: "/energy-crisis-alzheimers-brain/",
   plugins: [react()],
 });
+
 
